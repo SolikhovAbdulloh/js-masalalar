3cf1eb6248d52ce7a80a67b6b7b106c7df097077
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-20 07:50:57 +0500

    js

---
+++ main.js
@@ -0,0 +1,11 @@
+//1-masala
+
+// function sleepIn(weekday, vacation) {
+//   return !weekday || vacation;
+// }
+
+// 2-masala
+
+// function monkeyTrouble(aSmile, bSmile) {
+//   return (aSmile && bSmile) || (!aSmile && !bSmile);
+// }
